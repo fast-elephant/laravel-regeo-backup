15d3664f11a9fcabb27cfe918f8ab12138d7abfb
miaoxz <2444947559@qq.com>
2021-09-24 10:24:59 +0800

    get distance

--- src/Regeo.php
+++ src/Regeo.php
@@ -47,6 +47,21 @@ class Regeo
         }
     }
 
+    public function getDistance($lat1, $lng1, $lat2, $lng2)
+    {
+        $EARTH_RADIUS = 6378.137;
+
+        $radLat1 = $this->rad($lat1);
+        $radLat2 = $this->rad($lat2);
+        $a = $radLat1 - $radLat2;
+        $b = $this->rad($lng1) - $this->rad($lng2);
+        $s = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2)));
+        $s = $s * $EARTH_RADIUS;
+        $s = round($s * 10000) / 10000;
+
+        return $s;
+    }
+
     public function getLog()
     {
         return $this->log;
@@ -202,5 +217,10 @@ class Regeo
     {
         $this->log['responses'][] = $response;
         $this->log['requests']    += 1;
+    }
+
+    private function rad($d)
+    {
+        return $d * M_PI / 180.0;
     }
 }
